d87b54b53cf5c3959e91dbad317ffdc0122ad95d
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-03-23 23:32:49 +0800

    add a simple web server file to train with server and routing

---
+++ simple-web-server.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// //////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  //   console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my own header": "hello-world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests in port 8000");
+});
+
+// ROUTING
